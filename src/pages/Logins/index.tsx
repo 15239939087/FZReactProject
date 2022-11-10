0a14d55ee13468adab792bb5ddb3663daa61d4a2
峰至 <cw378892@digital-engine.com>
2022-11-10 15:54:56 +0800

    集成jest单测

--- src/pages/Logins/index.tsx
+++ src/pages/Logins/index.tsx
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
 
-type Props = {
-  extends: {
-    [key: string]: string;
+export default function App() {
+  const [func, setFunc] = useState(() => () => alert(1));
+
+  useEffect(() => {
+    getData();
+  }, []);
+  const getData = async () => {
+    const p1 = Promise.resolve("p1");
+
+    const p2 = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve("p2 延时一秒");
+      }, 1000);
+    });
+
+    const p3 = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve("p3 延时两秒");
+      }, 2000);
+    });
+    const res = await Promise.all([p1, p2, p3]);
+    console.log(res);
   };
-};
-const Logins = (props: Props) => {
-  console.log(props, 8989);
-  return (
-    <>
-      <div>123</div>
-    </>
-  );
-};
 
-export default Logins;
+  return (
+    <div className="App">
+      <button onClick={func}>执行方法</button>
+    </div>
+  );
+}
